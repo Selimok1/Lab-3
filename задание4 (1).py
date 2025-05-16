d048fdd014844aa88da2c337a82a08f1ff7eaa1f
Selim <ypselimok29@gmail.com>
2025-05-16 23:33:33 +0300

    задание4 (1).py

--- задание4 (1).py
+++ задание4 (1).py
@@ -1,16 +1,9 @@
 class BankAccount:
-    """
-    Класс банковского счета с инкапсуляцией.
-    """
-
     def __init__(self):
         self.__balance = 0
         self.__transactions = []
 
     def deposit(self, amount):
-        """
-        Внесение средств.
-        """
         if amount > 0:
             self.__balance += amount
             self.__transactions.append(('deposit', amount))
@@ -18,9 +11,6 @@ class BankAccount:
             print("Ошибка: сумма должна быть положительной.")
 
     def withdraw(self, amount):
-        """
-        Снятие средств.
-        """
         if 0 < amount <= self.__balance:
             self.__balance -= amount
             self.__transactions.append(('withdraw', amount))
@@ -29,19 +19,12 @@ class BankAccount:
 
     @property
     def balance(self):
-        """
-        Геттер для баланса.
-        """
         return self.__balance
 
     def get_transactions(self):
-        """
-        Получение списка транзакций.
-        """
         return self.__transactions
 
 
-# Пример использования
 if __name__ == "__main__":
     account = BankAccount()
     account.deposit(100)
