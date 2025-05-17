99f6f00c7315b54a7a60f8d2e10d6652cb75d41c
Selim <ypselimok29@gmail.com>
2025-05-17 12:36:28 +0300

    задание4 (1).py

--- задание4 (1).py
+++ задание4 (1).py
@@ -1,17 +1,17 @@
 class BankAccount:
     def __init__(self):
-        self.__balance = 0
+        self.__balance = 0  #Закрытый атрибут для хранения текущего баланса
         self.__transactions = []
 
     def deposit(self, amount):
         if amount > 0:
             self.__balance += amount
-            self.__transactions.append(('deposit', amount))
+            self.__transactions.append(('deposit', amount))  # Добавление транзакции в историю
         else:
             print("Ошибка: сумма должна быть положительной.")
 
     def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
+        if 0 < amount <= self.__balance:  # Проверка, что сумма положительная, меньше или равна текущему балансу
             self.__balance -= amount
             self.__transactions.append(('withdraw', amount))
         else:
@@ -25,7 +25,7 @@ class BankAccount:
         return self.__transactions
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Главная точка входа программы
     account = BankAccount()
     account.deposit(100)
     account.withdraw(30)
